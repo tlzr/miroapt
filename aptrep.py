e23a8fb56bf4e70a694bea891d9ac63b234fbc49
Ivan Udovichenko <tlzr@zmailbox.ru>
2017-06-21 19:32:38 +0300

    Rework the process of copying the source list

--- aptrep.py
+++ aptrep.py
@@ -3,25 +3,36 @@
 
 import apt
 import os
+import re
+import shutil
 
 class repository:
     def __init__(self):
         self.somevariable = []
 
-    def prepare_cache(self, path_to_cache, version):
-        cache = apt.cache.Cache(rootdir=path_to_cache)
-        path_to_sources_list = os.path.join(path_to_cache, 'etc/apt/sources.list')
-        if os.path.exists(path_to_sources_list) and os.stat(path_to_sources_list).st_size == 0:
-            if version == 'debian':
-                sources = """
-#------------------------------------------------------------------------------#
-#                            Debian REPOS                                      #
-#------------------------------------------------------------------------------#
-"""
+    def prepare_cache(self, path_to_cache_dir='cache', distr):
+        if not distr:
+            raise 'Please provide distribution name'
+        for root, dirs, files in os.walk('sources.list.d'):
+            if distr+'.list' not in files:
+                print('The list {0} is not in {1} directory.').format(distr+'.list', root)
+                raise
+            source_list_file = os.path.joinroo(root, distr+'.list')
 
-            with open(path_to_sources_list, 'w') as file:
-                file.write(sources)
-                file.close()
+        cache_dir = os.join(path_to_cache_dir)
+        if os.path.exists(cache_dir):
+            question = raw_input('Directory is already there. Do you want to 
+                                  remove it ?\n')
+            if re.match('(y|yes)', question, re.IGNORECASE):
+                try:
+                    shutils.rmtree(cache_dir)
+                except:
+                    print('Unexpacted error:', sys.exc_info()[0])
+                    raise
+
+        cache = apt.cache.Cache(rootdir=cache_dir)
+        path_to_sources_list = os.path.join(cache_dir, 'etc/apt/sources.list')
+        shutil.copyfile(source_list_file, path_to_sources_list)
 
         cache.clear()
         cache.update()
@@ -29,20 +40,20 @@ class repository:
 
         return cache
 
-    def prepare_apt(self, version, update_cache=False, cache_path='cache'):
+    def prepare_apt(self, distr, update_cache=False, cache_path='cache'):
         try:
             current_dir = os.getcwd()
         except:
             current_dir = './'
 
-        path_to_cache = os.path.join(current_dir, cache_path, version)
+        path_to_cache = os.path.join(current_dir, cache_path, distr)
 
         if update_cache:
-            cache = self.prepare_cache(path_to_cache, version)
+            cache = self.prepare_cache(path_to_cache, distr)
         else:
             cache = apt.cache.Cache(rootdir=path_to_cache)
             if len(cache) == 0:
-                cache = self.prepare_cache(path_to_cache, version)
+                cache = self.prepare_cache(path_to_cache, distr)
 
                 if len(cache) == 0:
                     print('Cache is still empty after update. Check sources list. Aborting.')
