0370e3603e8f9022afe6c39c8040443d41bff8d2
Ivan Udovichenko <tlzr@zmailbox.ru>
2017-06-22 17:12:37 +0300

    Fix class name, minor issues, add sources_list

--- aptrep.py
+++ aptrep.py
@@ -6,22 +6,22 @@ import os
 import re
 import shutil
 
-class repository:
+class Repository:
     def __init__(self):
         self.somevariable = []
 
-    def prepare_cache(self, path_to_cache_dir='cache', distr):
+    def prepare_cache(self, distr, path_to_cache_dir='cache'):
         if not distr:
             raise 'Please provide distribution name'
         for root, dirs, files in os.walk('sources.list.d'):
             if distr+'.list' not in files:
                 print('The list {0} is not in {1} directory.').format(distr+'.list', root)
                 raise
-            source_list_file = os.path.joinroo(root, distr+'.list')
+            source_list_file = os.path.join(root, distr+'.list')
 
-        cache_dir = os.join(path_to_cache_dir)
+        cache_dir = os.path.join(path_to_cache_dir)
         if os.path.exists(cache_dir):
-            question = raw_input('Directory is already there. Do you want to 
+            question = raw_input('Directory is already there. Do you want to \
                                   remove it ?\n')
             if re.match('(y|yes)', question, re.IGNORECASE):
                 try:
@@ -32,10 +32,13 @@ class repository:
 
         cache = apt.cache.Cache(rootdir=cache_dir)
         path_to_sources_list = os.path.join(cache_dir, 'etc/apt/sources.list')
-        shutil.copyfile(source_list_file, path_to_sources_list)
+        try:
+            shutil.copyfile(source_list_file, path_to_sources_list)
+        except:
+            print('Unexpacted error:', sys.exc_info()[0])
+            raise
 
-        cache.clear()
-        cache.update()
+        cache.update(fetch_progress=True,sources_list=path_to_sources_list)
         cache.open()
 
         return cache
