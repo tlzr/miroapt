8782d4a465aae03cd1525bfbe211c43eff5f3c50
Ivan Udovichenko <tlzr@zmailbox.ru>
2017-06-22 20:14:01 +0300

    Re-work process with cache

--- aptrep/aptrep.py
+++ aptrep/aptrep.py
@@ -2,15 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import apt
+import logging
 import os
 import re
 import shutil
 
 class Repository:
-    def __init__(self):
-        self.somevariable = []
 
-    def prepare_cache(self, distr, path_to_cache_dir='cache', update_cache=True):
+    def prepare_cache(self, distr, path_to_cache_dir='cache', force_cache_update=False, preserve_cache=True, key_path=None):
         if not distr:
             raise 'Please provide distribution name'
         for root, dirs, files in os.walk('sources.list.d'):
@@ -21,29 +20,45 @@ class Repository:
 
         cache_dir = os.path.join(path_to_cache_dir)
         if os.path.exists(cache_dir):
-            question = raw_input('Directory is already there. Do you want to \
-                                  remove it ?\n')
-            if re.match('(y|yes)', question, re.IGNORECASE):
+            if not preserve_cache:
                 try:
+                    logging.warning('Cache dir: {0} — is going to be deleted.').format(cache_dir)
                     shutils.rmtree(cache_dir)
                 except:
                     print('Unexpacted error:', sys.exc_info()[0])
                     raise
             else:
-                print('Cache is not going to be updated.')
+                logging.warning('Cache is not going to be updated.')
                 update_cache=False
+        else:
+            cache = apt.cache.Cache(rootdir=cache_dir)
 
-        cache = apt.cache.Cache(rootdir=cache_dir)
-        path_to_sources_list = os.path.join(cache_dir, 'etc/apt/sources.list')
-        try:
-            shutil.copyfile(source_list_file, path_to_sources_list)
-        except:
-            print('Unexpacted error:', sys.exc_info()[0])
-            raise
+        if key_path:
+            path_to_trusted.gpg.d = os.path.join(cache_dir, 'etc/apt/trusted.gpg.d')
+            try:
+                if not os.path.exists(path_to_trusted.gpg.d):
+                    os.mkdir(path_to_trusted.gpg.d)
+                #shutil.copyfile(key, path_to_trusted.gpg.d)
+            except:
+                print('Unexpacted error:', sys.exc_info()[0])
+                raise
+        else:
+           logging.notice('The key has not been provided.')
 
-        if update_cache:
-            cache.update(fetch_progress=True,sources_list=path_to_sources_list)
+        if force_cache_update:
+            path_to_sources_list = os.path.join(cache_dir, 'etc/apt/sources.list')
+            cache.update(fetch_progress=True,sources_list=source_list_file)
+
         cache.open()
 
         return cache
+
+    def remove_cache(cache_dir):
+        if os.path.exists(cache_dir):
+            try:
+                logging.warning('Cache dir: {0} — is going to be deleted.').format(cache_dir)
+                shutils.rmtree(cache_dir)
+            except:
+                print('Unexpacted error:', sys.exc_info()[0])
+                raise
 
