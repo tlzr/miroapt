ebc40ef6f7d55343a7b95c42ada2bc6d39e099f3
Ivan Udovichenko <tlzr@zmailbox.ru>
2017-06-23 19:38:18 +0300

    Work with lists of keys and sources

--- aptrep/aptrep.py
+++ aptrep/aptrep.py
@@ -6,59 +6,93 @@ import logging
 import os
 import re
 import shutil
+import urllib
+
 
 class Repository:
+    def prepare_cache(self, cache_dir='cache', sources_lists='/etc/apt/sources.list', keys='/etc/apt/trusted.gpg', force_cache_update=False):
+#        path_to_sources_list = os.path.join(cache_dir, 'etc/apt/sources.list.d')
 
-    def prepare_cache(self, distr, path_to_cache_dir='cache', force_cache_update=False, preserve_cache=True, key_path=None):
-        if not distr:
-            raise 'Please provide distribution name'
-        for root, dirs, files in os.walk('sources.list.d'):
-            if distr+'.list' not in files:
-                print('The list {0} is not in {1} directory.').format(distr+'.list', root)
-                raise
-            source_list_file = os.path.join(root, distr+'.list')
+        if not os.path.exists(cache_dir):
+            apt.cache.Cache(rootdir=cache_dir)
+            force_cache_update=True
 
-        cache_dir = os.path.join(path_to_cache_dir)
-        if os.path.exists(cache_dir):
-            if not preserve_cache:
-                try:
-                    logging.warning('Cache dir: {0} — is going to be deleted.').format(cache_dir)
-                    shutils.rmtree(cache_dir)
-                except:
-                    print('Unexpacted error:', sys.exc_info()[0])
-                    raise
-            else:
-                logging.warning('Cache is not going to be updated.')
-                update_cache=False
-        else:
-            cache = apt.cache.Cache(rootdir=cache_dir)
-
-        if key_path:
-            path_to_trusted.gpg.d = os.path.join(cache_dir, 'etc/apt/trusted.gpg.d')
-            try:
-                if not os.path.exists(path_to_trusted.gpg.d):
-                    os.mkdir(path_to_trusted.gpg.d)
-                #shutil.copyfile(key, path_to_trusted.gpg.d)
-            except:
-                print('Unexpacted error:', sys.exc_info()[0])
-                raise
-        else:
-           logging.notice('The key has not been provided.')
-
-        if force_cache_update:
+        path_to_apt = os.path.join(cache_dir, 'etc/apt')
+        if sources_lists:
             path_to_sources_list = os.path.join(cache_dir, 'etc/apt/sources.list')
-            cache.update(fetch_progress=True,sources_list=source_list_file)
+            try:
+                if isinstance(sources_lists, list):
+                    for sources_list in sources_lists:
+                        if not os.path.exists(path_to_sources_list):
+                            os.makedirs(path_to_sources_list)
+                        shutil.copy2(sources_list, path_to_sources_list)
+                else:
+                        shutil.copy2(sources_lists, path_to_sources_list)
+            except:
+                        print('Unexpacted error:', os.sys.exc_info()[0])
+                        raise
 
-        cache.open()
+        if keys:
+            path_to_trusted_gpg_d = os.path.join(cache_dir, 'etc/apt/trusted.gpg.d')
+            try:
+                if not os.path.exists(path_to_trusted_gpg_d):
+                    os.makedirs(path_to_trusted_gpg_d)
+                if isinstance(keys, list):
+                    for key in keys:
+                        if key.startswith('http'):
+                            if self.download(key, path_to_trusted_gpg_d):
+                        else:
+                            shutil.copy2(key, path_to_trusted_gpg_d)
+                else:
+                    shutil.copy2(keys, path_to_trusted_gpg_d)
+            except:
+                print('Unexpacted error:', os.sys.exc_info()[0])
+                raise
+        else:
+           logging.warning('The key has not been provided.')
+
+        cache = apt.cache.Cache(rootdir=cache_dir)
+        if force_cache_update:
+            cache.update()
+
+        cache.open(None)
 
         return cache
 
-    def remove_cache(cache_dir):
+    def remove_cache(self, cache_dir):
         if os.path.exists(cache_dir):
             try:
-                logging.warning('Cache dir: {0} — is going to be deleted.').format(cache_dir)
-                shutils.rmtree(cache_dir)
+                print('Cache dir: {0} — is going to be deleted.').format(cache_dir)
+                shutil.rmtree(cache_dir)
             except:
-                print('Unexpacted error:', sys.exc_info()[0])
+                print('Unexpacted error:', os.sys.exc_info()[0])
                 raise
+
+    def download(self, url, save_to_path):
+            save_to_path = os.path.join(save_to_path, filename)
+            url = sanitize_link(url)
+            if url:
+                filename = get_file_from_link(url)
+            if filename:
+                try:
+                    urllib.urlretrieve(url, filename=filename)
+                except:
+                    print('Unexpacted error:', os.sys.exc_info()[0])
+                    raise
+                return True
+            return False
+
+    def get_file_from_link(self, url)
+        if url:
+            url.split('/')
+            filename = url.splot('/')[-1]
+        if filename:
+            return filename
+        return None
+
+    def sanitize_link(self, url):
+        url = url.strip()
+        if url:
+            return url
+        return None
 
